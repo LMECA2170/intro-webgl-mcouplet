3a4cc91f0d7efafef1dd515b68c5bc84394d92b5
Mattéo Couplet <matteo.couplet@uclouvain.be>
2022-09-29 14:32:57 +0200

    Finish colorful square

--- app.js
+++ app.js
@@ -2,20 +2,23 @@
 const vertexShaderSource = `
 	precision mediump float;
 	attribute vec2 coordinates;
+	attribute vec3 aColor;
+
+	varying lowp vec3 vColor;
 
 	void main() {
 		gl_Position = vec4(coordinates, 0.0, 1.0);
+		vColor = aColor;
 	}
 `;
 
 const fragmentShaderSource = `
 	precision mediump float;
 
-	varying lowp vec4 color;
+	varying lowp vec3 vColor;
 
 	void main() {
-		gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-		
+		gl_FragColor = vec4(vColor, 1.0);
 	}
 `;
 
@@ -57,10 +60,10 @@ function main() {
 
 	// Define colors (one for each vertex of the square)
 	const colors = [
-		1.0, 1.0, 1.0, 1.0, // white
-		1.0, 0.0, 0.0, 1.0, // red
-		0.0, 1.0, 0.0, 1.0, // green
-		0.0, 0.0, 1.0, 1.0, // blue
+		1.0, 1.0, 1.0, // white
+		1.0, 0.0, 0.0, // red
+		0.0, 1.0, 0.0, // green
+		0.0, 0.0, 1.0, // blue
 	];
 
 	// SETUP SHADERS
@@ -74,19 +77,22 @@ function main() {
 	gl.linkProgram(program);
 	gl.useProgram(program);
 
-	// SETUP BUFFERS
+	// SETUP BUFFERS AND SHADER ATTRIBUTES
+	// Vertex positions
 	const vertexBuffer = gl.createBuffer(); // create empty buffer object
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); // bind array buffer
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW); // convert to 32bit array
-
-	const colorBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-	// SETUP SHADER ATTRIBUTES
 	var coord = gl.getAttribLocation(program, "coordinates");
 	gl.vertexAttribPointer(coord, 2, gl.FLOAT, gl.FALSE, 0, 0);
 	gl.enableVertexAttribArray(coord);
+
+	// Vertex colors
+	const colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	var colorAttribLocation = gl.getAttribLocation(program, "aColor");
+	gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, gl.FALSE, 0, 0);
+	gl.enableVertexAttribArray(colorAttribLocation);
 
 	// DRAW TRIANGLE
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, nVertices);
